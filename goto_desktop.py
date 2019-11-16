397dfc24f29c8086d7df77d45450e7e6c6879d3b
Larphien <denutrenne69@gmail.com>
2019-11-16 09:46:32 +0100

    Correction of an error when using the mouse to change of worspace when using multiple monitors

--- goto_desktop.py
+++ goto_desktop.py
@@ -26,13 +26,17 @@ def goto_desk(i3con, desk_id):
     i3con (i3ipc.Connection): the open connection with i3.
     id_desk (int): the id of the desk we want to go to.
     """
-    desk_name = i3con.get_workspaces()[desk_id]['name']
+    workspaces = i3con.get_workspaces()
+    workspaces.sort(key=lambda x: x['num'])
+    desk_name = workspaces[desk_id]['name']
     i3con.command("workspace {}".format(desk_name))
 
 
 if __name__ == '__main__':
     i3con = i3ipc.Connection()
     parser = argparse.ArgumentParser()
-    parser.add_argument('desktop_id', type=int, help="The id of the desktop we ant to go to")
+    parser.add_argument('desktop_id',
+                        type=int,
+                        help="The id of the desktop we want to go to")
     args = parser.parse_args()
     goto_desk(i3con, args.desktop_id)
